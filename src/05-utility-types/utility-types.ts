02c4819d3f364cd550d62ddd48844e1f9c06d116
Michael Steinert <Michael.Steinert.94@gmail.com>
2022-11-26 20:52:20 +0100

    Added Utility Types

--- src/05-utility-types/utility-types.ts
+++ src/05-utility-types/utility-types.ts
@@ -48,3 +48,45 @@ console.log(
     { id: '43', name: 'Marie' },
   ])
 );
+
+type Name = {
+  first: string;
+  last: string;
+};
+
+const addFullName = (name: Name): Name & { fullName: string } => {
+  return {
+    ...name,
+    fullName: `${name.first} ${name.last}`,
+  };
+};
+
+function permuteRows<T extends (...args: any[]) => any>(
+  iteratorFunction: T,
+  data: Parameters<T>[0][]
+): ReturnType<T>[] {
+  return data.map(iteratorFunction);
+}
+
+console.log(permuteRows(addFullName, [{ first: 'Michael', last: 'Steinert' }]));
+
+class PersonWithFullName {
+  constructor(public name: Name) {}
+
+  get fullName(): string {
+    return `${this.name.first} ${this.name.last}`;
+  }
+}
+
+function createObjects<T extends new (...args: any[]) => any>(
+  ObjectType: T,
+  data: ConstructorParameters<T>[0][]
+): InstanceType<T>[] {
+  return data.map((item) => new ObjectType(item));
+}
+
+console.log(
+  createObjects(PersonWithFullName, [
+    { first: 'Michael', last: 'Steinert' },
+  ]).map((obj: PersonWithFullName) => obj.fullName)
+);
